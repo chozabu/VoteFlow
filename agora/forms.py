44621c22f578ff4dbfbc1581eba405e29cde550b
Chozabu <chozabu@gmail.com>
2015-09-05 16:55:47 +0100

    added missing files

---
+++ agora/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Topic, Post, Tag, Representation, PostVote, TagVote
+from django.contrib.auth.models import User
+
+class TopicForm(forms.Form):
+	topic_parent_id = forms.ModelChoiceField(label='Parent Topic', queryset = Topic.objects.all(),required=False)
+	topic_name = forms.CharField(label='Topic Name', max_length=100)
+    
+
+class PostForm(forms.Form):
+	topic_id = forms.ModelChoiceField(label='Parent Topic', queryset = Topic.objects.all())
+	post_parent_id = forms.ModelChoiceField(label='Parent Post', queryset = Post.objects.all(),required=False)
+	post_name = forms.CharField(label='Message', max_length=100)
+	
+class PostVoteForm(forms.Form):
+	post_id = forms.ModelChoiceField(label='Parent',queryset = Post.objects.all())
+	vote_value = forms.FloatField(label='Vote (between 0 and 1, eg: .5)', max_value=1, min_value=0)
+	
+class RepresentationForm(forms.Form):
+	topic_id = forms.ModelChoiceField(label='Topic', queryset = Topic.objects.all())
+	representitive_id = forms.ModelChoiceField(label='Representitive', queryset = User.objects.all())
+	
+	
