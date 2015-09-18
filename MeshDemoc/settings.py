53026f58c193918aad84e911a3c29ec768ab3fb7
Chozabu <chozabu@gmail.com>
2015-09-18 01:53:10 +0100

    associate_by_email

--- MeshDemoc/settings.py
+++ MeshDemoc/settings.py
@@ -134,6 +134,19 @@ AUTHENTICATION_BACKENDS = (
    'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.social_auth.associate_by_email',
+    'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details'
+)
+
 SOCIAL_AUTH_FACEBOOK_KEY = localsettings['SOCIAL_AUTH_FACEBOOK_KEY']
 SOCIAL_AUTH_FACEBOOK_SECRET = gl('SOCIAL_AUTH_FACEBOOK_SECRET')
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = gl('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
