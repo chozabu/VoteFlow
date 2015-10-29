4a5a555d5ed940d243ba7f78b6effd57b6f81659
Chozabu <chozabu@gmail.com>
2015-10-29 22:46:55 +0000

    basic UI generation for json query objects.

--- agora/static/agora/js/filtering.js
+++ agora/static/agora/js/filtering.js
@@ -44,12 +44,14 @@ function postfilterbox_changed(event){
         strbox.hide();
         tagbox.show();
         console.log("doing TAG change")
-        tagbox.find('#tagfilterbox').change();
-        $('#tagfilterbox').change();
-        console.log("doing TAG change")
+        //tagbox.find('#tagfilterbox').change();
+        tagbox.find('#tagfilterbox').trigger('change');
+        //$('#tagfilterbox').change();
+        console.log("done TAG change")
     }
 }
 function tagfilterbox_changed(event){
+    console.log("TFT")
     //floatbox.hide();
     var mainbox = getfilteritem($(this))
     var newval = $(this)[0].value;
@@ -91,6 +93,9 @@ function get_filters(fcontainername, fboxname){
     //console.log(filters);
     return filters;
     //  mainbox.find('#outbox')[0].textContent=JSON.stringify(filter);
+}
+function remove_filter(event){
+    $(this).parent().remove();
 }
 function print_filter(event){
     var mainbox = $(this).parent().parent()
@@ -150,8 +155,57 @@ function get_filter(mainbox){
     rval[first+"__"+second]=filterval
     return rval;
 }
+function set_filter(mainbox, items, value){
+    console.log("mainbox");
+    console.log(mainbox);
+    console.log(mainbox.find('#postfilterbox'));
 
-$('#check').click(print_filter);
+    var first = items[0];
+    var second = "";
+    var third = "";
+    if (items.length)
+        second = items[1];
+    if (items.length>1)
+        third = items[2];
+    console.log("ITEMS:", first, second, third)
+
+    mainbox.find('#postfilterbox')[0].value = first;
+    mainbox.find('#postfilterbox').trigger('change');
+    if( $.inArray( first, [ "liquid_value", "liquid_sum",
+    "direct_value", "direct_sum" ] ) != -1){
+        mainbox.find('#floatfilterbox')[0].value = second;
+        mainbox.find('#floatfilterval')[0].value = value;
+    }
+    else if( $.inArray( first, [ "name", "text" ] ) != -1){
+        mainbox.find('#strfilterbox')[0].value = second;
+        mainbox.find('#strfilterval')[0].value = value;
+    }
+    else if( $.inArray( first, [ "created_at" ] ) != -1){
+        mainbox.find('#datefilterbox')[0].value = second;
+        mainbox.find('#datefilterval')[0].value = value;
+    }
+    else if( $.inArray( first, [ "tag" ] ) != -1){
+        mainbox.find('#tagfilterbox')[0].value = second;
+        //mainbox.find('#datefilterval')[0].value;
+        //setout(first+second:filterval};
+        if( $.inArray( second, [ "liquid_value", "liquid_sum",
+        "direct_value", "direct_sum" ] ) != -1){
+            mainbox.find('#floatfilterbox')[0].value = third;
+            mainbox.find('#floatfilterval')[0].value = value;
+        }
+        else if( $.inArray( second, [ "name", "text" ] ) != -1){
+            mainbox.find('#strfilterbox')[0].value = third;
+            mainbox.find('#strfilterval')[0].value = value;
+        }
+        else if( $.inArray( second, [ "created_at" ] ) != -1){
+            mainbox.find('#datefilterbox')[0].value = third;
+            mainbox.find('#datefilterval')[0].value = value;
+        }
+    }
+}
+
+//$('#check').click(print_filter);
+$('#removefilter').click(remove_filter);
 $('#postfilterbox').change(postfilterbox_changed);
 $('#tagfilterbox').change(tagfilterbox_changed);
 
@@ -171,18 +225,24 @@ function new_filter_action(foe){
     newfilter[0].id="afilter";
     newfilter.show();
     $(foe).append(newfilter);
+    return newfilter;
 }
-function new_filter_from_json(jin){
+function new_filter_from_json(jin, foe){
     console.log("new filter");
     console.log($('#filterproto'));
+    foe = '#currentfiltersbox';
+    newfilter = new_filter_action(foe);
     //get the key and value object
+    var query = "";
+    var val = "";
+    for (k in jin){
+        query=k;
+        val=jin[k];
+    }
     //split key by __s?
+    var qparts = query.split("__");
+    set_filter(newfilter, qparts, val);
     //same structure as getfilter, but setting values
-    var newfilter = $('#filterproto').clone(true);
-    newfilter.find('#postfilterbox').change();
-    newfilter[0].id="afilter";
-    newfilter.show();
-    $('#currentfiltersbox').append(newfilter);
 }
 $('#newfilterbutton').click(new_filter);
 $('#newexcludebutton').click(new_exclude);
